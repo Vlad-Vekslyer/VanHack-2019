d5239ddf51a5cacfb03dc9424a14800aefd9d40c
Shota Senga <1989.shota@gmail.com>
2019-09-15 13:49:46 -0700

    delete backend directory

--- backendDDD/models/Walk.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Walk = new mongoose.Schema({
-  time: Date,
-  walkers: [{type: mongoose.Schema.Types.ObjectId, ref:"user"}],
-  dog: {type: mongoose.Schema.Types.ObjectId, ref: "dog"}
-})
-
-module.exports = mongoose.model('walk', Walk);
